64eee0d800cbece4fc092f50db2aaffbe0af4178
TryLoess <18359072052@163.com>
2025-10-26 11:41:25 +0800

    最小测试

--- st_test.py
+++ st_test.py
@@ -2,5 +2,6 @@ import requests
 import streamlit as st
 
 if __name__ == "__main__":
+    st.info("测试是否能连接上外网,访问url为http://43.136.22.64:5050/paddle_test_data，方式为post")
     response = requests.post("http://43.136.22.64:5050/paddle_test_data")
-    st.info(f"回复的code为{response.status_code}")
+    st.info(response.status_code)
